c7a62c6e069fe78446f11795f9e72b833d694968
20167x <34091745+20167x@users.noreply.github.com>
2017-12-21 12:09:57 +0100

    Add files via upload
    http://20167.hosts.ma-cloud.nl/opdracht5/

---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+
+$to = '20167@ma-web.nl';
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = 'From: ' . $_POST['from'];
+
+
+mail($to, $subject, $message, $headers);
+
+echo 'Uw mail is verstuurd. Bedankt!';
+
+
+?>
